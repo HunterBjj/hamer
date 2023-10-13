df9994c2ec9c159ed49dc7c444fc9ef5ba34d814
Bjj <a.urmanow@mail.ru>
2023-10-13 17:43:24 +0700

    problem main.html

--- hammer_systems/user_code/urls.py
+++ hammer_systems/user_code/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.user, name='user'),
     path('auth2.html/', views.reg, name='reg'),
-    path('main.html/', views.data_db, name='data_db'),
+    path('main.html/', views.create, name='create'),
 ]
